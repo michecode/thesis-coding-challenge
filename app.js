5c82273613f89708134e04b40b07ca65492dda0e
maddy <maddy.fries@protonmail.com>
2021-12-20 00:47:54 -0800

    yeah

---
+++ app.js
@@ -0,0 +1,15 @@
+export function mobileDropdown() {
+    console.log("slay");
+    // toggle icon
+    
+    var x = document.getElementById("mobile-dropdown");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+} 
+
+export function eyeToggle() {
+    
+}
